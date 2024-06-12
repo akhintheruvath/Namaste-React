b81e72983bc1e788d7f23ca94c8e09eedb8cff88
Akhin <akhintheruvath2827@gmail.com>
2024-06-12 22:12:18 +0530

    writing nested elements using React.createElement()

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
-const heading = React.createElement(
-   "h1",
-   { id: "heading", xyz: "abc" },
-   "Hello world from React"
+const parent = React.createElement(
+   "div",
+   {
+      id: "parent",
+   },
+   [
+      React.createElement(
+         "div",
+         {
+            id: "child",
+         },
+         [
+            React.createElement(
+               "h1",
+               {},
+               "I'm an h1 tag"
+            ),
+            React.createElement(
+               "h2",
+               {},
+               "I'm an h2 element"
+            ),
+         ]
+      ),
+      React.createElement(
+         "div",
+         {
+            id: "child2"
+         },
+         [
+            React.createElement(
+               "h1",
+               {},
+               "I'm an h1 tag"
+            ),
+            React.createElement(
+               "h2",
+               {},
+               "I'm an h2 tag"
+            ),
+         ]
+      ),
+   ]
 );
-console.log("heading: ", heading); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
