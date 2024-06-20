7b51a255f881f7cd5cbaf5f131af976e65118a9c
Akhin <akhintheruvath2827@gmail.com>
2024-06-20 11:56:11 +0530

    JSX syntax

--- App.js
+++ App.js
@@ -9,7 +9,12 @@ const heading = React.createElement(
 );
 
 // creating React element with JSX
-const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+// Parcel transpiles this JSX code before it reaches the JS engine
+const jsxHeading = (
+   <h1 className="head" tabIndex="5">
+     Namaste React using JSX
+   </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
