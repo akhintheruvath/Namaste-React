2f8a49a428f5a0dae5c484d690792d2729bd27fe
Akhin <akhintheruvath2827@gmail.com>
2024-06-20 12:36:45 +0530

    Component composition

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// creating React element with React.createElement()
-const heading = React.createElement(
-   "h1",
-   { id: "heading" },
-   "Namaste React"
-);
-
-// creating React element with JSX
-// Parcel transpiles this JSX code before it reaches the JS engine
-const jsxHeading = (
+// React Element
+const Title = () => (
    <h1 className="head" tabIndex="5">
      Namaste React using JSX
    </h1>
 );
 
+// Component composition
+const Heading = () => {
+   return (
+      <div>
+         <Title />
+         <h1>Namaste React Functional Component</h1>
+      </div>
+   );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading />);
